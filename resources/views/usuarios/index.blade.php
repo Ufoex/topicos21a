8565e142b6d078e306275a9d1caf1c51e0d4dd9c
Ufoex <masterllamas@hotmail.com>
2021-05-10 14:20:31 -0500

    Fix user create

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -5,12 +5,11 @@
         <div class="card">
             <div class="card-header">
                 <h2 class="float-left">{{__('custom.list-user')}}</h2>
-                <a href="{{route('register')}}">
+                <a href="{{route('usuarios')}}">
                     <button type="button" class="btn btn-success float-right">{{__('custom.add-user-button')}}</button>
                 </a>
             </div>
             <div class="card-body">
-
                 <table class="table table-hover">
                     <thead>
                     <tr>
@@ -18,7 +17,6 @@
                         <th scope="col">{{__('custom.name')}}</th>
                         <th scope="col">{{__('custom.email')}}</th>
                         <th scope="col">{{__('custom.actions')}}</th>
-
                     </tr>
                     </thead>
                     <tbody>
@@ -35,14 +33,35 @@
                                     @csrf
                                     @method('DELETE')
                                     <button type="submit" class="btn btn-danger">Eliminar</button>
+                                    <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                                        Launch demo modal
+                                    </button>
                                 </form>
                             </td>
                         </tr>
                     @endforeach
                     </tbody>
-
                 </table>
             </div>
         </div>
+        <!-- Modal -->
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        ...
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+                </div>
+            </div>
     </div>
 @endsection
