7bc9c5ce58759657298b604a1c57ebe157c5e833
Ufoex <masterllamas@hotmail.com>
2021-05-10 13:05:42 -0500

    Soporte de ingles y espanol a las views

--- resources/lang/es/custom.php
+++ resources/lang/es/custom.php
@@ -7,5 +7,9 @@ return [
     'password' => 'Contraseña',
     'update-button' => 'Actualizar',
     'cancel-button' => 'Cancelar',
-
+    'list-user' => 'Lista de Usuarios',
+    'actions' => 'Acciones',
+    'delete-button' => 'Eliminar',
+    'add-user-button' => 'Agregar Usuario',
+    'edit-button' => 'Editar',
 ];
